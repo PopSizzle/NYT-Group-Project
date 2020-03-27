946119e119470f6a1a7a57a81ed3eb3b45bb918b
Gibbyswag <will.gibson7@gmail.com>
2020-03-27 12:15:36 -0700

    checked AJAX API call is working and defined search filter variables with input from html

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 $(document).ready(function() {
+    //input from form 
+    var searchTerm = $("#searchInput").val().trim();
+    //input from form 
+    var numberRecords = $("#recordsInput").val().trim();
+    //override from input form 
+    var beginYear = 1851
+    
+    if ($("#beginYearInput").val().trim()) {
+        beginYear = $("#beginYearInput").val().trim()
+    }; 
 
-    var searchTerm = "" //input from form
-    var numberRecords = "" //input from form
-    var beginYear = 1851 //override from from
-    var endYear = 2020 //override from from
-    var sortMethod = newest
+    //override from input form 
+    var endYear = 2020
 
-    $(document).on("click", ".class", function() {
-        
-      var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTopic + "&begin_date=" + beginYear + "0101&end_date=" + endYear + "1231&api-key=d9wLAp3TYiJAnSURQOH9BYGkKKUvwwS1";
+    if ($("#endYearInput").val().trim()) {
+        endYear = $("#endYearInput").val().trim()
+    };
+
+    // $(document).on("click", "#test", function() {
+      function runSearch() {
+
+      var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + searchTerm + "&begin_date=" + beginYear + "0101&end_date=" + endYear + "1231&sort=newest&api-key=d9wLAp3TYiJAnSURQOH9BYGkKKUvwwS1";
   
       $.ajax({
         url: queryURL,
         method: "GET"
       })
         .then(function(response) {
+            console.log(response)
           
             for ( var i = 0; i < numberRecords; i++) {
 
-                //
+                //Create divs for response
 
             };
         });
-    });
+    };
+    
+    runSearch()
 });
+// });
